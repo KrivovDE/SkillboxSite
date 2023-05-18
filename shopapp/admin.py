4b92aa692621785c3fc735f8307d0b4978d22fb5
Dmitrii Krivov <d.krivov@kitcup.ru>
2023-05-18 13:51:21 +0300

    4.4 Отображение и редактирование связанных записей

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from shopapp.models import Product
+from shopapp.models import Product, Order
 
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [OrderInline]
     list_display = 'pk', 'name', 'description_short', 'price', 'discount'
     list_display_links = 'pk', 'name'
     ordering = 'pk',
@@ -16,4 +20,19 @@ class ProductAdmin(admin.ModelAdmin):
         return f'{obj.description[:48]}...'
 
 
-# admin.site.register(Product, ProductAdmin) -- так нужно сделать если не используешь декоратор на 6стр
+# admin.site.register(Product, ProductAdmin) -- так нужно сделать если не используешь декоратор
+
+class ProductInline(admin.TabularInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [ProductInline]
+    list_display = 'delivery_address', 'promocode', 'created_at', 'user_verbose'
+
+    def get_queryset(self, request):
+        return Order.objects.select_related('user').prefetch_related('products')
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
